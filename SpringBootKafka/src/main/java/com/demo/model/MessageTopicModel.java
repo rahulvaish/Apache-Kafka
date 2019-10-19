850c6a3309820c93dd577cd7df6db53e8db2bff7
Rahul Vaish <vaish.rahul4810@gmail.com>
2019-10-19 07:28:23 +0530

    Add files via upload

---
+++ SpringBootKafka/src/main/java/com/demo/model/MessageTopicModel.java
@@ -0,0 +1,29 @@
+package com.demo.model;
+
+public class MessageTopicModel {
+	private String topic;
+	private String message;
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "MessageTopicModel [topic=" + topic + ", message=" + message + "]";
+	}
+
+	
+}
