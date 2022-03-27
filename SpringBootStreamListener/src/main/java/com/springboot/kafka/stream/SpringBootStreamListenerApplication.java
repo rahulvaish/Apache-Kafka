2abc4b0f05ae0739793ee95fc66cb73c07889a5e
rahulvaish <rahulvaish@Rahuls-MacBook-Pro.local>
2022-03-27 17:48:44 -0400

    kafka listener

---
+++ SpringBootStreamListener/src/main/java/com/springboot/kafka/stream/SpringBootStreamListenerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.kafka.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStreamListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStreamListenerApplication.class, args);
+	}
+
+}
