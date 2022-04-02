b1aa48af600e0d0761a099fd9cbce053f2e08f35
rahulvaish <rahulvaish@amazon-fef364754.chateau-lincoln.local>
2022-04-02 16:00:30 -0400

    Kafka Producer

--- KafkaProducerConsumer/src/main/java/com/example/KafkaProducerConsumer/Producer.java
+++ KafkaProducerConsumer/src/main/java/com/example/KafkaProducerConsumer/Producer.java
@@ -2,36 +2,31 @@ package com.example.KafkaProducerConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 public class Producer {
 
 	public static void main(String[] args) throws InterruptedException {
-		 String topic = "topic82";
-	        String brokers = "localhost:8082";
+		 String topic = "foobar";
+	        String brokers = "localhost:9092";
+		    String IntegerSerializer = "org.apache.kafka.common.serialization.IntegerSerializer";
 	        String StringSerializer = "org.apache.kafka.common.serialization.StringSerializer";
-
-	        Random rnd = new Random();
 
 	        Map<String, Object> config = new HashMap<String, Object>();
 	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
-	        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer);
+	        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer);
 	        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer);
 
+	        KafkaProducer<Integer,String> producer = new KafkaProducer<>(config);
 
-	        KafkaProducer producer = new KafkaProducer<String, String>(config);
-
-	        while (true) {
-	            LocalDateTime runtime = LocalDateTime.now();
-	            String msg = runtime + " "+ rnd.nextInt(255);
-	            ProducerRecord record = new ProducerRecord<String, String>(topic, msg);
-	            producer.send(record);
-	            Thread.sleep(100);
+			System.out.println("PUBLISHING STARTED");
+	        for(int i=0;i< 10;i++){
+	            producer.send(new ProducerRecord(topic, i, "MSG#"+i));
 	        }
-
+			producer.close();
+			System.out.println("PUBLISHING ENDED");
 	}
 
 }
