9463a2a6b3dd8ed5c9ff04b4cbb4d813a3d43b2f
rahulvaish <rahulvaish@Rahuls-MacBook-Pro.local>
2022-04-02 19:22:38 -0400

    MT

--- KafkaProducerMultiThrreading/src/main/java/com/example/KafkaProducerConsumer/KafkaProducerMultiThrreading.java
+++ KafkaProducerMultiThrreading/src/main/java/com/example/KafkaProducerConsumer/KafkaProducerMultiThrreading.java
@@ -9,28 +9,47 @@ import java.util.Map;
 public class KafkaProducerMultiThrreading {
 
 	public static void main(String[] args) throws InterruptedException {
-		    String topic = "foobar";
-	        String brokers = "localhost:9092";
-		    String IntegerSerializer = "org.apache.kafka.common.serialization.IntegerSerializer";
-	        String StringSerializer = "org.apache.kafka.common.serialization.StringSerializer";
+		    
+		String topic = "foobar";
+        String brokers = "localhost:9092";
+	    String IntegerSerializer = "org.apache.kafka.common.serialization.IntegerSerializer";
+        String StringSerializer = "org.apache.kafka.common.serialization.StringSerializer";
 
-	        Map<String, Object> config = new HashMap<String, Object>();
-	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
-	        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer);
-	        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer);
+        Map<String, Object> config = new HashMap<String, Object>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer);
 
-	        KafkaProducer<Integer,String> producer = new KafkaProducer<>(config);
+        KafkaProducer<Integer,String> producer = new KafkaProducer<>(config);
 
-			System.out.println("APP STARTED");
-			try {
-				Thread t1 = new Thread(new Dispatcher(producer,topic),"Thread#1");
-				Thread t2 = new Thread(new Dispatcher(producer,topic),"Thread#2");
-				
-				t1.start();
-				t2.start();
-			}catch(Exception e) {
-				
-			}
+		System.out.println("APP STARTED");
+		
+		Thread t1 = new Thread(new Dispatcher(producer,topic),"Thread#1");
+		Thread t2 = new Thread(new Dispatcher(producer,topic),"Thread#2");
+		t1.start();
+		try
+        {
+            t1.join();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Exception has been caught" + ex);
+        }
+  
+        t2.start();
+        try
+        {
+            t2.join();
+        }
+  
+        catch(Exception ex)
+        {
+            System.out.println("Exception has been caught" + ex);
+        }
+		finally {
+			producer.close();
+		}
+		
 	}
 
 }
